77bcd5ac729c32b9cac4b57a7d3d8b5da957db9d
beyond9thousand <beyond9thousand@gmail.com>
2022-06-12 04:49:10 +0530

    added rule for open tablet driver

--- .config/bspwm/rules/bspc-rules.sh
+++ .config/bspwm/rules/bspc-rules.sh
@@ -35,7 +35,7 @@ for i in "${settings[@]}"; do
 declare -a floats=(Alafloat Lxappearance Arandr \
 Viewnior Pcmanfm feh Nm-connection-editor Matplotlib \
 Yad ColorPicker Gnome-disks SimpleScreenRecorder \
-Font-manager Gnome-system-monitor)
+Font-manager Gnome-system-monitor OpenTabletDriver.UX.Gtk)
 for i in "${floats[@]}"; do
   bspc rule -a $i manage=on state=floating follow=on focus=on centre=true; done
 
