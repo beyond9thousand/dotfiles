b6603e109b89b4e3ea281e121f1592bb32c4347a
beyond9thousand <beyond9thousand@gmail.com>
2022-06-14 18:37:15 +0530

    added perfect screen dimming

--- .config/bspwm/hooks/b9k-keyled.sh
+++ .config/bspwm/hooks/b9k-keyled.sh
@@ -8,11 +8,13 @@ while true; do
   # echo "$idleTimeMillis"  # just for debug purposes.
   if [[ $idle1 = false && $idleTimeMillis -gt $idleAfter ]] ; then
     msi-perkeyrgb -s 000000
+    xbacklight -dec 30
     idle1=true
   fi
 
   if [[ $idle1 = true && $idleTimeMillis -lt $idleAfter ]] ; then
     msi-perkeyrgb -s aa0000
+    xbacklight -inc 30
     idle1=false
   fi
   sleep 1      # polling interval
