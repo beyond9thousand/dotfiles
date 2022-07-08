ef010cc2171cdb40e24b03ff58ce85f852f2af57
beyond9thousand <beyond9thousand@gmail.com>
2022-07-08 15:00:07 +0530

    nice toggle modules

--- .config/bspwm/hooks/b9k-keyled.sh
+++ .config/bspwm/hooks/b9k-keyled.sh
@@ -6,35 +6,42 @@ warntime=270000
 idle=false
 stage1=false
 stage2=false
+lock="/tmp/light-toggle.lock"
 
 while true; do
-	idletime=$(xprintidle)
-	# echo "$idletime"  # just for debug purposes.
-	if [[ $idle = false && $idletime -gt $dimtime ]]; then
-		msi-perkeyrgb -s 000000
-		og=$(xbacklight -get)
-		xbacklight -set 5
-		idle=true
-		stage1=true
-	fi
+  idletime=$(xprintidle)
+  # echo "$idletime"  # just for debug purposes.
+  if [[ $idle = false && $idletime -gt $dimtime ]]; then
+    msi-perkeyrgb -s 000000
+    if [[ ! -e $lock ]]; then
+      og=$(xbacklight -get)
+      xbacklight -set 5
+    fi
+    idle=true
+    stage1=true
+  fi
 
-	if [[ $stage1 = true && $idle = true && $idletime -gt $warntime ]]; then
-		pkill -9 -f "dunst"
-		dunstify -i screensaver "Screensaver" "Screen will turn off in 30 seconds"
-		stage1=false
-		stage2=true
-	fi
+  if [[ $stage1 = true && $idle = true && $idletime -gt $warntime ]]; then
+    pkill -9 -f "dunst"
+    dunstify -i screensaver "Screensaver" "Screen will turn off in 30 seconds"
+    stage1=false
+    stage2=true
+  fi
 
-	if [[ $stage2 = true && $idle = true && $idletime -gt $offtime ]]; then
-		xbacklight -set 0
-		stage2=false
-	fi
+  if [[ $stage2 = true && $idle = true && $idletime -gt $offtime ]]; then
+    if [[ ! -e $lock ]]; then
+      xbacklight -set 0
+    fi
+    stage2=false
+  fi
 
-	if [[ $idle = true && $idletime -lt $dimtime ]]; then
-		msi-perkeyrgb -s aa0000
-		xbacklight -set "$og"
-		idle=false
-	fi
-	sleep 1 # polling interval
+  if [[ $idle = true && $idletime -lt $dimtime ]]; then
+    msi-perkeyrgb -s aa0000
+    if [[ ! -e $lock ]]; then
+      xbacklight -set "$og"
+    fi
+    idle=false
+  fi
+  sleep 1 # polling interval
 
 done
