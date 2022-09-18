34bfeca1e905cdc8d42d1a5ebfea29992a7bdbc0
beyond9thousand <beyond9thousand@gmail.com>
2022-09-18 17:27:38 +0530

    passing more variables now

--- .config/eww/shell/notif_state.zsh
+++ .config/eww/shell/notif_state.zsh
@@ -1,28 +1,30 @@
 #!/usr/bin/env zsh
 
+switch (){
+    if [[ $(dunstctl is-paused | grep true) ]]; then
+        dunstctl set-paused false &>/dev/null
+        eww update notif_initial=""
+        dunstify --replace 659 -i bell "Notifications" "DND mode deactivated"
+    else
+        dunstify -C 659
+        dunstctl set-paused true &>/dev/null
+        eww update notif_initial=""
+    fi
+}
+
+check (){
+    if [[ $(dunstctl is-paused | grep true) ]]; then
+        echo "" | awk '{print}'
+    else
+        echo "" | awk '{print}'
+    fi
+}
+
 case "$1" in
     -s|--switch)
-        function switch (){
-            if [[ $(dunstctl is-paused | grep true) ]]; then
-                dunstctl set-paused false &>/dev/null
-                eww update notif_initial=""
-                dunstify --replace 659 -i bell "Notifications" "DND mode deactivated"
-            else
-                dunstify -C 659
-                dunstctl set-paused true &>/dev/null
-                eww update notif_initial=""
-            fi
-        }
         switch
         ;;
     -c|--check)
-        function check (){
-            if [[ $(dunstctl is-paused | grep true) ]]; then
-                echo "" | awk '{print}'
-            else
-                echo "" | awk '{print}'
-            fi
-        }
         check
         ;;
 esac
--- .config/eww/shell/sound_state.zsh
+++ .config/eww/shell/sound_state.zsh
@@ -2,49 +2,55 @@
 
 mic_source=$(pactl info | grep "Default Source" | awk '{print$3}')
 
+vol_master(){
+    pamixer ${1}
+    pactl subscribe |
+    grep --line-buffered "Event 'change' on ${2} " |
+    while read -r _; do
+        pamixer ${1} | cut -d " " -f1
+    done
+}
+
+mic_state(){
+    if [[ $(amixer get Capture | grep -i "\[on]") ]]; then
+        echo "false" | awk '{print}'
+    else
+        echo "true" | awk '{print}'
+    fi
+}
+
+mic_toggle() {
+    if [[ $(amixer get Capture | grep -i "\[on]") ]]; then
+        eww update mic_initial="true"
+        pactl set-source-mute @DEFAULT_SOURCE@ true
+    else
+        eww update mic_initial="false"
+        pactl set-source-mute @DEFAULT_SOURCE@ false
+    fi
+}
+
+mic_volume (){
+    pamixer --source $mic_source --get-volume
+    pactl subscribe |
+    grep --line-buffered "Event 'change' on source " |
+    while read -r _; do
+        pamixer --source $mic_source --get-volume
+    done
+}
+
 case "$1" in
     -v | --volume)
-        function volume() {
-            pamixer --get-volume
-            pactl subscribe |
-            grep --line-buffered "Event 'change' on sink " |
-            while read -r _; do
-                pamixer --get-volume | cut -d " " -f1
-            done
-        }
-        volume | stdbuf -oL -eL uniq | cat
+        vol_master "--get-volume" "sink" | \
+            stdbuf -oL -eL uniq | cat
         ;;
     -s | --state)
-        function state() {
-            pamixer --get-mute
-            pactl subscribe |
-            grep --line-buffered "Event 'change' on sink " |
-            while read -r _; do
-                pamixer --get-mute
-            done
-        }
-        state | stdbuf -oL -eL uniq | cat
+        vol_master "--get-mute" "sink" | \
+            stdbuf -oL -eL uniq | cat
         ;;
     -mc|--mic_check)
-        function mic_state(){
-            if [[ $(amixer get Capture | grep -i "\[on]") ]]; then
-                echo "false" | awk '{print}'
-            else
-                echo "true" | awk '{print}'
-            fi
-        }
         mic_state
         ;;
     -mt|--mic_toggle)
-        function mic_toggle() {
-            if [[ $(amixer get Capture | grep -i "\[on]") ]]; then
-                eww update mic_initial="true"
-                pactl set-source-mute @DEFAULT_SOURCE@ true
-            else
-                eww update mic_initial="false"
-                pactl set-source-mute @DEFAULT_SOURCE@ false
-            fi
-        }
         mic_toggle
         ;;
     -mic_up)
@@ -54,14 +60,6 @@ case "$1" in
         pamixer --source $mic_source -d 5
         ;;
     -mic_vol)
-        function mic_volume (){
-            pamixer --source $mic_source --get-volume
-            pactl subscribe |
-            grep --line-buffered "Event 'change' on source " |
-            while read -r _; do
-                pamixer --source $mic_source --get-volume
-            done
-        }
         mic_volume | stdbuf -oL -eL uniq | cat
         ;;
 esac
