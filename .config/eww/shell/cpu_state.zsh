7951391c403eb0d4cd5bb7bd86f9338aa77fac26
beyond9thousand <beyond9thousand@gmail.com>
2022-09-26 02:35:41 +0530

    simplifying scripts

--- .config/eww/shell/cpu_state.zsh
+++ .config/eww/shell/cpu_state.zsh
@@ -1,8 +1,19 @@
 #!/usr/bin/env zsh
 
+DEVICE=$(for i in /sys/class/hwmon/hwmon*/temp*_input; \
+        do echo "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null \
+        || echo $(basename ${i%_*})) $(readlink -f $i)"; done \
+    | grep -w "pch_cannonlake" | awk -F 'temp1 ' '{print$2}')
+
+cpu_temp(){
+    while true; do
+        awk '{print($1/1000)}' $DEVICE
+        sleep 15
+    done
+}
+
 case "$1" in
     -t | --temp)
-        echo $(($(cat /sys/devices/virtual/thermal/thermal_zone1/hwmon4/temp1_input)/1000)) \
-            | awk '{print}'
+        cpu_temp
         ;;
 esac
