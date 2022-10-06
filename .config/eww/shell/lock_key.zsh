103ad8a71faa03d0ba8a8083c0c6bdc407985594
beyond9thousand <beyond9thousand@gmail.com>
2022-10-07 04:30:42 +0530

    friday morning cleanup

--- .config/eww/shell/lock_key.zsh
+++ .config/eww/shell/lock_key.zsh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 
+_sound="/usr/share/sounds/Pop/stereo/notification"
+_count=0
+
 xkb -s1 | while read _ caps_lock num_lock; do
     old_val=$num_lock
     if [[ $num_lock == "off" && $old_val != $new_val ]]; then
@@ -10,4 +13,12 @@ xkb -s1 | while read _ caps_lock num_lock; do
         dunstify -C 696
     fi
     echo "{\"caps_lock\":\"$caps_lock\",\"num_lock\":\"$num_lock\"}"
+    if [[ $_count != 0 ]]; then
+        if [[ $caps_lock == "on" || $num_lock == "off" ]]; then
+            paplay $_sound/device-added.oga &
+        else
+            paplay $_sound/device-removed.oga &
+        fi
+    fi
+    ((_count++))
 done
