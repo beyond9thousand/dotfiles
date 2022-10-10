6f4a8a5357443f7067f5ce3c13039f3c8d81fd4c
beyond9thousand <beyond9thousand@gmail.com>
2022-10-10 15:14:05 +0530

    monday morning checkup

--- .config/eww/shell/lock_key.zsh
+++ .config/eww/shell/lock_key.zsh
@@ -2,6 +2,9 @@
 
 _sound="/usr/share/sounds/Pop/stereo/notification"
 _count=0
+_print(){
+    echo "{\"caps_lock\":\"$CAPS\",\"num_lock\":\"$NUM\"}"
+}
 
 xkb -s1 | while read _ CAPS NUM; do
     num_old=$NUM
@@ -12,15 +15,15 @@ xkb -s1 | while read _ CAPS NUM; do
         num_new=$num_old
         caps_new=$caps_old
 
-        if [[ $NUM == "off" ]]; then
-            dunstify -r 696 -t 5000 -u critical "Report" "Numlock was turned OFF"
-        else
-            dunstify -C 696
-        fi
-
-        echo "{\"caps_lock\":\"$CAPS\",\"num_lock\":\"$NUM\"}"
+        _print
 
         if [[ $_count != 0 ]]; then
+            if [[ $NUM == "off" ]]; then
+                dunstify -r 696 -t 5000 -u critical "Report" "Numlock was turned OFF"
+            else
+                dunstify -C 696
+            fi
+
             if [[ $CAPS == "on" || $NUM == "off" ]]; then
                 paplay $_sound/device-added.oga &
             else
