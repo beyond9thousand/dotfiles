5659aae2a47c5152a67649788982f827442e6df8
beyond9thousand <beyond9thousand@gmail.com>
2022-10-13 02:51:51 +0530

    packed commands into functions, key indicator

--- .config/eww/shell/lock_key.zsh
+++ .config/eww/shell/lock_key.zsh
@@ -1,36 +1,46 @@
 #!/usr/bin/env zsh
 
+# choose a sound theme folder of your choice
 _sound="/usr/share/sounds/Pop/stereo/notification"
-_count=0
+
 _print(){
     echo "{\"caps_lock\":\"$CAPS\",\"num_lock\":\"$NUM\"}"
 }
 
-xkb -s1 | while read _ CAPS NUM; do
-    num_old=$NUM
-    caps_old=$CAPS
+_detect(){
+    if [[ $_count != 0 ]]; then
+        if [[ $NUM == "off" ]]; then
+            dunstify -r 696 -t 5000 -u critical "Report" "Numlock was turned OFF"
+        else
+            dunstify -C 696
+        fi
 
+        if [[ $CAPS == "on" || $NUM == "off" ]]; then
+            paplay $_sound/device-added.oga &
+        else
+            paplay $_sound/device-removed.oga &
+        fi
+    fi
+}
 
+_stale(){
     if [[ $num_old != $num_new || $caps_old != $caps_new ]]; then
         num_new=$num_old
         caps_new=$caps_old
 
-        _print
-
-        if [[ $_count != 0 ]]; then
-            if [[ $NUM == "off" ]]; then
-                dunstify -r 696 -t 5000 -u critical "Report" "Numlock was turned OFF"
-            else
-                dunstify -C 696
-            fi
-
-            if [[ $CAPS == "on" || $NUM == "off" ]]; then
-                paplay $_sound/device-added.oga &
-            else
-                paplay $_sound/device-removed.oga &
-            fi
-        fi
+        _print; _detect
     fi
+}
+
+# initial run check variable
+_count=0
+
+# begin monitoring
+xkb -s1 | while read _ CAPS NUM; do
+    num_old=$NUM
+    caps_old=$CAPS
+
+    _stale
 
     _count=1
 done
